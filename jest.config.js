73e1947b448839b985ccd6263a3c8415e80e4bcf
Mark Tucker <mark@shazaml.com>
2019-06-21 08:29:00 -0700

    Change to Project structure and tsconfig

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     "json",
     "node",
   ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
