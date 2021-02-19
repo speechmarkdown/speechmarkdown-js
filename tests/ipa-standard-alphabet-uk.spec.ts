51d7d164d6738881512e0ad4d79ae443a6f1efb3
Song Li <songl@outlook.com>
2021-02-19 17:49:41 +0800

    test phoneme tag

--- tests/ipa-standard-alphabet-uk.spec.ts
+++ tests/ipa-standard-alphabet-uk.spec.ts
@@ -58,6 +58,22 @@ describe('ipa-standard-alphabet-uk', () => {
     expect(ssml).toBe(expected);
   });
 
+  test('converts to SSML - Microsoft Azure', () => {
+
+    const options = {
+      platform: 'microsoft-azure'
+    };
+    const ssml = speech.toSSML(markdown, options);
+
+    const expected = dedent`
+      <speak>
+      I say, <phoneme alphabet="ipa" ph="ˈˌb.d.f.g.h.j.k.l.m.n.p.s.t.v.w.z.i.u.æ.ð.ʃ.θ.ʒ.ə.aɪ.aʊ.ɑ.eɪ.ɝ.ɛ.ɪ.əʊ.ɔ.ɔɪ.ʊ.ʌ.ɒ.ɛə.ɪə.ʊə.ŋ.ɹ.d͡ʒ.t͡ʃ">ipa</phoneme>.
+      </speak>
+    `;
+
+    expect(ssml).toBe(expected);
+  });
+
   test('converts to Plain Text', () => {
 
     const options = {
