7b84272890c58a3e86a39887ac7edb65f6c61c99
Song Li <songl@outlook.com>
2021-02-19 16:32:58 +0800

    test break tag

--- tests/break-time.spec.ts
+++ tests/break-time.spec.ts
@@ -58,6 +58,22 @@ describe('break-time', () => {
     expect(ssml).toBe(expected);
   });
 
+  test('converts to SSML - Microsoft Azure', () => {
+
+    const options = {
+      platform: 'microsoft-azure'
+    };
+    const ssml = speech.toSSML(markdown, options);
+
+    const expected = dedent`
+      <speak>
+      Sample <break time="3s"/> speech <break time="250ms"/> markdown
+      </speak>
+    `;
+
+    expect(ssml).toBe(expected);
+  });
+
   test('converts to Plain Text', () => {
 
     const options = {
